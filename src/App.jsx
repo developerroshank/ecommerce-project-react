8685a8c3d636579ad506bbc5d142347bb97d1a41
developerroshank <developerroshank@gmail.com>
2025-08-15 20:11:09 +0530

    Add Homepage in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Header } from './components/Header'
+import { HomePage } from './pages/HomePage'
 
 const App = () => {
   return (
     <>
-      <Header />
+      <HomePage />
     </>
   )
 }
