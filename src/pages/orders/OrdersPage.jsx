5b25d541fb69295336ed257ab6fcd10000de8cde
developerroshank <developerroshank@gmail.com>
2025-09-07 23:22:59 +0530

    Convert orders page to async await and remove static orders-container

--- src/pages/orders/OrdersPage.jsx
+++ src/pages/orders/OrdersPage.jsx
@@ -11,10 +11,12 @@ export const OrdersPage = ({ cart }) => {
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/orders?expand=products')
-      .then((response) => {
-        setOrders(response.data);
-      });
+    const fetchOrdersData = async () => {
+      const response = await axios.get('/api/orders?expand=products');
+      setOrders(response.data);
+    }
+
+    fetchOrdersData();
   }, []);
 
   return (
@@ -88,57 +90,6 @@ export const OrdersPage = ({ cart }) => {
               </div>
             );
           })}
-
-          <div className="order-container">
-
-            <div className="order-header">
-              <div className="order-header-left-section">
-                <div className="order-date">
-                  <div className="order-header-label">Order Placed:</div>
-                  <div>June 10</div>
-                </div>
-                <div className="order-total">
-                  <div className="order-header-label">Total:</div>
-                  <div>$41.90</div>
-                </div>
-              </div>
-
-              <div className="order-header-right-section">
-                <div className="order-header-label">Order ID:</div>
-                <div>b6b6c212-d30e-4d4a-805d-90b52ce6b37d</div>
-              </div>
-            </div>
-
-            <div className="order-details-grid">
-              <div className="product-image-container">
-                <img src="images/products/intermediate-composite-basketball.jpg" />
-              </div>
-
-              <div className="product-details">
-                <div className="product-name">
-                  Intermediate Size Basketball
-                </div>
-                <div className="product-delivery-date">
-                  Arriving on: June 17
-                </div>
-                <div className="product-quantity">
-                  Quantity: 2
-                </div>
-                <button className="buy-again-button button-primary">
-                  <img className="buy-again-icon" src="images/icons/buy-again.png" />
-                  <span className="buy-again-message">Add to Cart</span>
-                </button>
-              </div>
-
-              <div className="product-actions">
-                <a href="tracking.html">
-                  <button className="track-package-button button-secondary">
-                    Track package
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </>
